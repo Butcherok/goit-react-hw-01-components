b3912005d5ff16e84a8ce8993cd6739277315dcf
Butcherok <butcherok231@gmail.com>
2023-03-16 23:55:33 +0200

    add user card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import user from 'data/user.json';
+// import data from 'data/data.json';
+// import friends from 'data/friends.json';
+// import transactions from 'data/transactions.json';
+import { Profile } from './user/Profile';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return <Profile user={user} />;
 };
